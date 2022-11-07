7ed9a248170ac379dab5f34c2ba6b3745c114cf8
MaxChou1205 <v2asault@yahoo.com.tw>
2022-11-07 22:22:59 +0800

    builder example and summary

---
+++ 02-Builder/example3.js
@@ -0,0 +1,49 @@
+class Field {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+class Class {
+  constructor(name) {
+    this.name = name;
+    this.fields = [];
+  }
+
+  toString() {
+    let buffer = [];
+    buffer.push(`class ${this.name} {\n`);
+    if (this.fields.length > 0) {
+      buffer.push(
+        `  constructor(${this.fields.map(f => f.name).join(", ")}) {\n`
+      );
+      for (let field of this.fields) {
+        buffer.push(`    this.${field.name} = ${field.name};\n`);
+      }
+      buffer.push("  }\n");
+    }
+    buffer.push("}");
+
+    return buffer.join("");
+  }
+}
+
+class CodeBuilder {
+  constructor(className) {
+    this._class = new Class(className);
+  }
+
+  addField(name) {
+    this._class.fields.push(new Field(name));
+    return this;
+  }
+
+  toString() {
+    return this._class.toString();
+  }
+}
+
+//
+let cb = new CodeBuilder("Person");
+cb.addField("name").addField("age");
+console.log(cb.toString());
